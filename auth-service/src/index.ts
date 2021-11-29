8c0729915d55e11b8a84d56cebe371be1abf28bb
Gunkar Singh <gunkar.singh@neofinancial.com>
2021-11-28 20:02:58 -0700

    Initial scaffolding for auth routes

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,8 +1,18 @@
 import express from "express"
 import { json } from "body-parser"
 
+import { currentUserRouter } from "./routes/current-user"
+import { signInRouter } from "./routes/signin"
+import { signOutRouter } from "./routes/signout"
+import { signUpRouter } from "./routes/signup"
+
 const app = express()
 app.use(json())
+
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
 
 app.listen(4000, () => {
     console.log("auth-service is listening on port 4000!")
