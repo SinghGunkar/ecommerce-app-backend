be1f3630cc0d9d49ec776e5cd4c5038c56b165e7
Gunkar Singh <gunkar.singh@neofinancial.com>
2021-12-04 18:43:03 -0700

    Created and accessing secrets in kubernetes

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express"
 import { body, validationResult } from "express-validator"
+import jwt from "jsonwebtoken"
+
 import { User } from "../models/user"
 import { RequestValdationError } from "../errors/request-validation-error"
 import { BadRequestError } from "../errors/bad-request-error"
@@ -38,6 +40,20 @@ router.post(
 
         const user = User.build({ email, password })
         await user.save()
+
+        // Generate jwt
+        const userJwt = jwt.sign(
+            {
+                id: user.id,
+                email: user.email
+            },
+            "mySecretOrPrivateKey"
+        )
+
+        // Store jwt on the session object
+        req.session = {
+            jwt: userJwt
+        }
 
         res.status(201).send(user)
     }
