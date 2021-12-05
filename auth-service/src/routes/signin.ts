fcc4687c3a83e9b06de6a7d9a41b73f26dd36758
Gunkar Singh <gunkar.singh@neofinancial.com>
2021-12-04 20:09:32 -0700

    Initial logic for signin route

--- auth-service/src/routes/signin.ts
+++ auth-service/src/routes/signin.ts
@@ -1,9 +1,27 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import { body, validationResult } from "express-validator"
+import { RequestValdationError } from "../errors/request-validation-error"
 
 const router = express.Router()
 
-router.post("/api/users/signin", (req, res) => {
-    res.send("sign in user")
-})
+router.post(
+    "/api/users/signin",
+    [
+        body("email").isEmail().withMessage("Email must be valid"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("You must supply a password")
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            throw new RequestValdationError(errors.array())
+        }
+
+        res.send({ message: "tryed to sign in" })
+    }
+)
 
 export { router as signInRouter }
